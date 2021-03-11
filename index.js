87c2724cf6b30f9fd3c8b68df3570ca8b92985a8
Paras Mani <Parasmani300@gmail.com>
2021-03-11 11:12:44 +0530

    2nd Commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function handleSubmit()
 {
-    var n = document.getElementById("marks").value;
-    document.getElementById("output").innerHTML = n;
+    var n1 = document.getElementById("marks1").value;
+    var n2 = document.getElementById("marks2").value;
+    var n3 = document.getElementById("marks3").value;
+    var n4 = document.getElementById("marks4").value;
+
+    var n = parseInt(n1) + parseInt(n2) + parseInt(n3) + parseInt(n4); 
+    var perc = (n/400)*100
+    document.getElementById("output").innerHTML = "Percentage = " + perc;
 }
 
 function handleClear()
